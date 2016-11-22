f0c7f87f7a1105868d4f8bc62f9b3d3abb261aa1
DBluff <douglas.bluff@austincc.edu>
2016-11-22 14:10:59 -0600

    Redirect Block modified for WordPress

--- RedirectBlock.php
+++ RedirectBlock.php
@@ -1,29 +1,12 @@
 <?php
 
-$db_host = "localhost";
-    $db_username = "root";
-    $db_password = "root";
-    $db_name = "sitecopy";
+$soCol = drupal_get_path_alias($path = NULL);
+$result = db_query("SELECT redirect.redirect, url_alias.source as node, redirect.source as vanity, url_alias.alias FROM redirect INNER JOIN url_alias ON redirect.redirect=url_alias.source WHERE url_alias.alias='$soCol'");
 
-    $sqldb = mysqli_connect($db_host, $db_username, $db_password, $db_name)
-    or die ('MySQL Not found // Could Not Connect.');
+$record = $result->fetchAll();
 
-    $fullTable = mysqli_query($sqldb, "
-SELECT redirect.redirect, url_alias.source, redirect.source, url_alias.alias
-FROM redirect
-INNER JOIN url_alias
-ON redirect.redirect=url_alias.source
-ORDER BY url_alias.source;;");
-    ?>
-
-    <table>
-    <?php
-
-    while ($row = mysqli_fetch_array($fullTable)) {
-
-        if ($_SERVER['REQUEST_URI'] === $row['alias']) {
-            echo 'Vanity URL:' . '<br />' . '<a href="http://austincc.edu/' . $row['source'] . '">' . 'austincc.edu/' . $row['source'] . '</a>';
-        } else exit();
-    }
+if($record[0]->vanity != '' && preg_match_all('/([\/])/', $record[0]->vanity)<2 && preg_match_all('/([\-])/', $record[0]->vanity)<2) {
+    print '<div class="VanURL"><strong>' . 'Vanity URL: </strong>' . '<a href="http://austincc.edu/' . ($record[0]->vanity) . '">' . 'austincc.edu/';
+    print_r($record[0]->vanity);
+    print '</a></div>';
 }
-
---
+++ TheoryCraft.php
@@ -0,0 +1,25 @@
+<?php
+
+//Call Current Page - get_permalink()
+$soCol = 'degrees-and-certificates/find-classes/honors';
+
+//Query Foreign Database
+$mydb = new wpdb('root','root','sitecopy','localhost');
+
+//Get Requisite Information
+$row = $mydb->get_results("SELECT redirect.redirect, url_alias.source as node, redirect.source as vanity, url_alias.alias FROM redirect INNER JOIN url_alias ON redirect.redirect=url_alias.source WHERE url_alias.alias='$soCol'");
+//Filter to Vanity URL
+$vanURL = array_map(create_function('$o', 'return $o->vanity;'), $row);
+
+//Filter Out Bad Vanity URLS (computer generated)
+if($vanURL[0] != '' && preg_match_all('/([\/])/', $vanURL[0])<2 && preg_match_all('/([\-])/', $vanURL[0])<2) {
+    print '<div class="VanURL"><strong>' . 'Vanity URL: </strong>' . '<a href="http://austincc.edu/' . $vanURL . '">' . 'austincc.edu/';
+    echo($vanURL[0]);
+    print '</a></div>';
+}
+
+
+
+
+
+
